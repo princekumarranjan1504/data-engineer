1b45f91a5dac63a05ac0cab04698e2957f206d81
princekumarranjan1504 <princekumarranjan1504@gmail.com>
2024-07-02 19:11:37 +0530

    added day 1 files.

---
+++ day1python/1.intro.py
@@ -0,0 +1,52 @@
+# WHAT IS PROGRAMMING?
+
+# Just like we use Hindi or English to communicate with each other, we use a
+# programming language like Python to communicate with the computer.
+# Programming is a way to instruct the computer to perform various tasks.
+
+
+# WHAT IS PYTHON?
+
+# Python is a simple and easy to understand language which feels like reading simple
+# English. This Pseudo code nature is easy to learn and understandable by beginners.
+
+
+# FEATURES OF PYTHON
+
+# Easy to understand = Less development time
+# Free and open source
+# High level language
+# Portable: Works on Linux / Windows / Mac.
+# Fun to work with!
+
+
+# INSTALLATION
+# Python can be easily installed from python.org. When you click on the download
+# button, python can be installed right after you complete the setup by executing the file
+# for your platform.
+
+# print function: it is used to print any thing in the display
+
+print("hello world") # print is a function (more later)
+
+# Execute this file (.py file) by typing python hello.py and you will see Hello World printed
+# on the screen.
+
+# COMMENTS
+
+# Comments are used to write something which the programmer does not want to
+# execute. This can be used to mark author name, date etc.
+
+
+# TYPES OF COMMENTS
+
+# There are two types of comments in python.
+# 1. Single Line Comments: To write a single line comment just add a ‘#’ at the start
+# of the line.
+
+# This is a Single-Line Comment
+
+# 2. Multiline Comments: To write multi-line comments you can use ‘#’ at each lineor you can use the multiline string (""" """)
+"""This is an amazing
+example of a Multiline
+comment!"""
---
+++ day1python/hw_ch1.py
@@ -0,0 +1,10 @@
+print('hello')
+import pyjokes
+
+joke = pyjokes.get_joke()
+print(joke)
+
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("I will speak this text")
+engine.runAndWait()

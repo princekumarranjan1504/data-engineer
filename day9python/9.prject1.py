5c45c9e2aca808dc06997ea783e1dbe298cec0a0
princekumarranjan1504 <princekumarranjan1504@gmail.com>
2024-07-06 00:58:31 +0530

    adding day 9 python file

---
+++ day9python/9.prject1.py
@@ -0,0 +1,44 @@
+import random
+'''
+1 for snake
+-1 for water
+0 for gun
+'''
+
+computer = random.choice([-1,0,1])
+youstr = input("Enter your choice: ")
+youDict = {
+    "s": 1,
+    "w": -1,
+    "g": 0
+}
+reverseDict={
+    1:"snake",
+    -1:"water",
+    0:"gun"
+}
+
+you = youDict[youstr]
+
+#By now we have 2 numbers (variables), you and computer 
+
+print(f'You chose {reverseDict[you]}\nComputer chose {reverseDict[computer]}')
+
+if(computer == you):
+    print('Its a draw')
+else:
+    if(computer == -1 and you == 1):
+        print('You win')
+    elif(computer == -1 and you == 0):
+        print('You Lose')
+    elif(computer == 1 and you == -1):
+        print('You Lose')
+    elif(computer == 1 and you == 0):
+        print('You Win')
+    elif(computer == 0 and you == 1):
+        print('You Lose')
+    elif(computer == 0 and you == -1):
+        print('You Win')
+    else:
+        print('Something went wrong!')
+

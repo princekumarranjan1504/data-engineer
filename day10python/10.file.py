f559240d40cc2beebcbd842b9be4ba27bb4679e3
princekumarranjan1504 <princekumarranjan1504@gmail.com>
2024-07-09 00:01:17 +0530

    adding day 10 python file

--- day10python/10.file.py
+++ day10python/10.file.py
@@ -1,1 +1,13 @@
-# FILE I/O
+# FILE I/O-A file is data stored in a storage device. A python program can talk to the file by reading
+# content from it and writing content to it.
+
+# There are 2 types of files:
+# 1. Text files (.txt, .c, etc)
+# 2. Binary files (.jpg, .dat, etc)
+
+# opening a files
+f=open("file.txt") #opening a file
+data = f.read() #reading a file
+print(data) #printing a file
+f.close() #closing a file
+
